cbd6e08b408851431040b8d9bd2a400cfecd9d31
Roman Kozin <romanko95.rk@gmail.com>
2022-05-06 09:42:36 +0300

    Config
    - Added caching configs

--- config/google.php
+++ config/google.php
@@ -6,5 +6,31 @@ return [
             'distance_matrix' => env('GOOGLE_DISTANCE_MATRIX_KEY')
         ]
     ],
-    'standalone' => []
+    'standalone' => [],
+    'caching' => [
+
+        /**
+         * Caching can be in two variants:
+         * 1. Boolean (true, false)
+         * 2. Array ([\KielD01\LaravelGoogleApi\Api\DistanceMatrix::class, ...])
+         * @see \KielD01\LaravelGoogleApi\Api\\KielD01\LaravelGoogleApi\Api\DistanceMatrix
+         *
+         * If array, then class must extend \KielD01\LaravelGoogleApi\Core::class
+         * @see \KielD01\LaravelGoogleApi\Core
+         */
+        'enabled' => env('GOOGLE_CACHING_ENABLED', false),
+
+        /**
+         * TTL - in seconds
+         * 60   - 1 Minute
+         * 300  - 5 Minutes
+         * 600  - 10 Minutes
+         * 900  - 15 Minutes
+         * 1800 - 30 Minutes
+         * 3600 - 1 Hour (default)
+         *
+         * 0    - forever
+         */
+        'ttl' => env('GOOGLE_CACHE_TTL', 3600),
+    ]
 ];
